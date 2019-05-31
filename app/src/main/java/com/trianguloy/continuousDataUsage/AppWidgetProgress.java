7dadb656915bdb3d1990a271ad5aed7fa6022fc6
TrianguloY <correo--correo@hotmail.com>
2019-05-31 19:55:32 +0200

    V 1.5 - Smaller 'average usage' bar (the top one) - Compact mode when the progress widget size is too small

--- app/src/main/java/com/trianguloy/continuousDataUsage/AppWidgetProgress.java
+++ app/src/main/java/com/trianguloy/continuousDataUsage/AppWidgetProgress.java
@@ -3,6 +3,7 @@ package com.trianguloy.continuousDataUsage;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.graphics.Point;
+import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.RemoteViews;
 
@@ -19,6 +20,20 @@ public class AppWidgetProgress extends AppWidgetBase {
      */
     static final int DEFAULT_PROGRESS_PRECISION = 512;
     
+    /**
+     * When the size changes, update the widget
+     * @param context          base context
+     * @param appWidgetManager widget manager, base class
+     * @param appWidgetId      id of the widget to update
+     * @param newOptions       unused
+     */
+    @Override
+    public void onAppWidgetOptionsChanged(Context context,
+                                          AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+    
+        updateAppWidget(context, appWidgetManager, appWidgetId);
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
     
     /**
      * Updates a widget adding its views and configuring them.
@@ -28,10 +43,14 @@ public class AppWidgetProgress extends AppWidgetBase {
      * @param appWidgetId      id of the widget to update
      */
     void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+    
+        boolean small = appWidgetManager.getAppWidgetOptions(appWidgetId).getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH) < 180;
         
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_progress);
+        RemoteViews views = new RemoteViews(context.getPackageName(), small ? R.layout.widget_progress_short : R.layout.widget_progress);
         updateViews(context, views);
+        
+        //change size of date elements
         
         //update when clicking
         setOnClick(context, new int[]{appWidgetId}, views, new int[]{R.id.wdg_prgBar_data, R.id.wdg_prgBar_date}, ACTION_REFRESH, AppWidgetProgress.class);
